50088c761ca40a7b60ea7c9bb0f7843627b0f6e4
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2024-09-07 21:00:49 -0400

    add admin route

---
+++ link-pay/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import {AdminDashboard} from '@/components/admin-dashboard'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <AdminDashboard />
+    </main>
+  )
+}
